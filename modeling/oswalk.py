6d1ed4161fb81b5829c60946416b65c135e81d59
chorsengi2r <71380382+chorsengi2r@users.noreply.github.com>
2023-04-04 16:44:56 +0800

    Update oswalk.py

--- modeling/oswalk.py
+++ modeling/oswalk.py
@@ -3,7 +3,7 @@ import os
 root_dir = "./../MATH"
 
 for subdir, dir, files in os.walk(root_dir):
-    print (root)
-    print (dirs)
+    print (subdir)
+    print (dir)
     print (files)
     print ('--------------------------------')
